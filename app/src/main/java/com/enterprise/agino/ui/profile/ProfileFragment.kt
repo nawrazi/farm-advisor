6e93a985a3335e4122aea64427dcbb4d720b7f6e
Abdulaziz Ali <s9uidward@gmail.com>
2023-02-11 21:47:09 +0300

    done with profile screen

---
+++ app/src/main/java/com/enterprise/agino/ui/profile/ProfileFragment.kt
@@ -0,0 +1,39 @@
+package com.enterprise.agino.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.enterprise.agino.R
+import com.enterprise.agino.databinding.FragmentNewFarmBinding
+import com.enterprise.agino.databinding.FragmentProfileBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
