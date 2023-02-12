5f2ceb190c8eaae294b284e88c8130877e891884
Bruk Tedla <biruk.tedla93@gmail.com>
2023-02-12 17:30:08 +0300

    add sign up fragment functionalities

--- app/src/main/java/com/enterprise/agino/ui/onboarding/phonenumbersignup/PhoneNumberSignUpFragment.kt
+++ app/src/main/java/com/enterprise/agino/ui/onboarding/phonenumbersignup/PhoneNumberSignUpFragment.kt
@@ -5,30 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.enterprise.agino.databinding.FragmentPhoneNumberSignupBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PhoneNumberSignUpFragment : Fragment() {
     private var _binding: FragmentPhoneNumberSignupBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by viewModels<PhoneNumberSignupViewModel>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPhoneNumberSignupBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
         setupListeners()
         return binding.root
     }
 
     private fun setupListeners() {
-        binding.apply {
-            continueBtn.setOnClickListener {
-                findNavController().navigate(PhoneNumberSignUpFragmentDirections.actionPhoneNumberSignUpFragmentToOnBoarding2Fragment())
-            }
+        binding.continueBtn.setOnClickListener {
+            findNavController().navigate(
+                PhoneNumberSignUpFragmentDirections.actionPhoneNumberSignUpFragmentToOnBoarding2Fragment(
+                    viewModel.countryCode + viewModel.phoneNumber
+                )
+            )
+        }
+
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
         }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
