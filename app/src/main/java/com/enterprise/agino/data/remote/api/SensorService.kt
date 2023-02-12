221ad9e7028fce7f9972ffcdce1f2e8acb13b4d1
hello_world <haileformal@gmail.com>
2023-02-12 16:22:40 +0300

    update retrofit sensor model

--- app/src/main/java/com/enterprise/agino/data/remote/api/SensorService.kt
+++ app/src/main/java/com/enterprise/agino/data/remote/api/SensorService.kt
@@ -1,12 +1,11 @@
 package com.enterprise.agino.data.remote.api
 
-import com.enterprise.agino.data.remote.dto.CreateUserRequest
+import com.enterprise.agino.data.remote.dto.AddSensorRequest
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface SensorService {
-    // TODO: change this to the correct model
     @GET("api/AddSensor")
-    suspend fun CreateSensor(): Response<CreateUserRequest>
+    suspend fun AddSensor(): Response<AddSensorRequest>
 
 }
