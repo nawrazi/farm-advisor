95bd11cec3a7bc6fbac19d868f3c99ced5035e56
Abdulaziz Ali <s9uidward@gmail.com>
2023-02-12 11:46:16 +0300

    added user request model

---
+++ app/src/main/java/com/enterprise/agino/data/remote/dto/UserRequest.kt
@@ -0,0 +1,12 @@
+package com.enterprise.agino.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class UserRequest(
+    val name: String,
+    val email: String,
+    val phone: String,
+
+    @SerializedName("authId")
+    val authID: String
+)
