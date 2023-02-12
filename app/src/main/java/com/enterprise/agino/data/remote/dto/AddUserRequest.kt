ed1fc5856c631381946d34ef8d210069884226cf
Abdulaziz Ali <s9uidward@gmail.com>
2023-02-12 13:31:44 +0300

    created farm request model

--- app/src/main/java/com/enterprise/agino/data/remote/dto/AddUserRequest.kt
+++ app/src/main/java/com/enterprise/agino/data/remote/dto/AddUserRequest.kt
@@ -2,7 +2,7 @@ package com.enterprise.agino.data.remote.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class UserRequest(
+data class AddUserRequest(
     val name: String,
     val email: String,
     val phone: String,
