30993e85f22893669ff5e4b1e97e888345cffd41
Bruk Tedla <biruk.tedla93@gmail.com>
2023-02-12 18:18:31 +0300

    implement sign in fragment functionalists

--- app/src/main/java/com/enterprise/agino/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/enterprise/agino/ui/signin/SignInFragment.kt
@@ -5,27 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.enterprise.agino.databinding.FragmentSignInBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignInFragment : Fragment() {
     private var _binding: FragmentSignInBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by viewModels<SignInViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSignInBinding.inflate(inflater, container, false)
-        setupListeners()
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
+        setupListeners()
+        setupUI()
         return binding.root
+    }
+
+    private fun setupUI() {
+        if (viewModel.countryCode.isNotEmpty())
+            binding.countryCodePicker.setCountryForPhoneCode(viewModel.countryCode.toInt())
     }
 
     private fun setupListeners() {
         binding.apply {
             continueBtn.setOnClickListener {
-                findNavController().navigate(SignInFragmentDirections.actionSignInFragmentToVerificationCodeFragment())
+                findNavController().navigate(
+                    SignInFragmentDirections.actionSignInFragmentToVerificationCodeFragment(
+                        viewModel!!.phoneNumber.value!!
+                    )
+                )
+            }
+
+            backButton.setOnClickListener {
+                findNavController().popBackStack()
+            }
+
+            countryCodePicker.setOnCountryChangeListener {
+                viewModel!!.countryCode = countryCodePicker.selectedCountryCode
             }
         }
     }
