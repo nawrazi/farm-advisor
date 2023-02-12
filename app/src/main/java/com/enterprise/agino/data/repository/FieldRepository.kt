dd6e73417d3b872ce7ccd62df632f2f190ea8479
hello_world <haileformal@gmail.com>
2023-02-12 23:01:38 +0300

    implement field repository

---
+++ app/src/main/java/com/enterprise/agino/data/repository/FieldRepository.kt
@@ -0,0 +1,25 @@
+package com.enterprise.agino.data.repository
+
+import com.enterprise.agino.common.Resource
+import com.enterprise.agino.common.networkBoundResource
+import com.enterprise.agino.data.mapper.toField
+import com.enterprise.agino.data.remote.api.FieldService
+import com.enterprise.agino.domain.model.Field
+import com.enterprise.agino.domain.repository.IFieldRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class FieldRepository @Inject constructor(
+    private val fieldService: FieldService
+) : IFieldRepository {
+    override fun GetField(id: String): Flow<Resource<Field>> {
+        return networkBoundResource(
+            fetch = {
+                fieldService.GetField(id).body()!!
+            },
+            mapFetchedValue = {
+                it.toField()
+            }
+        )
+    }
+}
