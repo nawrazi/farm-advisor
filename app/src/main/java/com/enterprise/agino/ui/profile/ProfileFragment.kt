669fd5338b0e479c0110b0461a72587672c2234d
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-02-12 06:20:56 +0300

    delete unnecessary variables

--- app/src/main/java/com/enterprise/agino/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/enterprise/agino/ui/profile/ProfileFragment.kt
@@ -9,16 +9,6 @@ import com.enterprise.agino.R
 import com.enterprise.agino.databinding.FragmentNewFarmBinding
 import com.enterprise.agino.databinding.FragmentProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
