dc89909d72f43436c9b1bb75b93c413868d2c2ef
Biruk <biruktassew21@gmail.com>
2023-02-14 01:16:02 +0300

    change base url

--- app/src/main/java/com/enterprise/agino/common/Constants.kt
+++ app/src/main/java/com/enterprise/agino/common/Constants.kt
@@ -3,7 +3,7 @@ package com.enterprise.agino.common
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object Constants {
-    const val BASE_URL = "http://10.6.250.142:7071/"
+    const val BASE_URL = "http://192.168.43.252:7071/"
     const val MAP_KEY = "QKG5sZdSQCFSitlPKDsIHZ9FV7ZGZg4g"
 
     val FARM_PREF_KEY by lazy { stringPreferencesKey("farm") }
