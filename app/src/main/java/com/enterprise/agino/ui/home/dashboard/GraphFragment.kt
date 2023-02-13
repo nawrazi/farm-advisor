03c4960c85e8dbae939d677ea6654c370a9836a1
hello_world <haileformal@gmail.com>
2023-02-13 16:06:34 +0300

    style temperature and precipitation graph

--- app/src/main/java/com/enterprise/agino/ui/home/dashboard/GraphFragment.kt
+++ app/src/main/java/com/enterprise/agino/ui/home/dashboard/GraphFragment.kt
@@ -2,13 +2,13 @@ package com.enterprise.agino.ui.home.dashboard
 
 import android.os.Bundle
 import android.text.InputType
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.util.component1
 import androidx.core.util.component2
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.enterprise.agino.R
 import com.enterprise.agino.databinding.FragmentGraphScreenBinding
 import com.enterprise.agino.domain.model.Sensor
@@ -17,18 +17,21 @@ import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.google.android.material.datepicker.*
 import com.google.android.material.textfield.TextInputEditText
+import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
+@AndroidEntryPoint
 class GraphFragment : Fragment() {
     private var _binding: FragmentGraphScreenBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: GraphViewModel by viewModels()
     private lateinit var adapter: SensorsAdapter
-    
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,9 +43,36 @@ class GraphFragment : Fragment() {
             setOnClickListener { showDatePicker(this) }
         }
 
+        viewModel.temperatureGraphDataSet.observe(viewLifecycleOwner) {
+            binding.chart1.apply {
+                data = LineData(it)
+                it.setColors(
+                    resources.getColor(R.color.royal_orange, null)
+                )
+                data.setDrawValues(false)
+                it.setDrawCircles(false)
+                it.lineWidth = 3f
+                it.mode = LineDataSet.Mode.CUBIC_BEZIER
+                styleLineGraph(this)
+                invalidate()
+            }
+        }
+
+        viewModel.precipitationGraphDataSet.observe(viewLifecycleOwner) {
+            binding.chart2.apply {
+                val snowDepth = it.component1()
+                val missingData = it.component2()
+                snowDepth.color = resources.getColor(R.color.navy_blue, null)
+                missingData.color = resources.getColor(R.color.light_grey, null)
+                data = BarData(listOf(snowDepth, missingData))
+
+                data.setDrawValues(false)
+                styleBarGraph(this)
+                invalidate()
+            }
+        }
+
         setupSensorAdapter()
-        setGraphData(binding.chart1)
-        setGraphData(binding.chart2)
         return binding.root
     }
 
@@ -53,58 +83,28 @@ class GraphFragment : Fragment() {
         val data = listOf(
             Sensor(
                 "45678987654", null, "", 0, 97,
-                "","", 0.0, 0.0, 0, ""
+                "", "", 0.0, 0.0, 0, ""
             ),
             Sensor(
                 "98765445678", null, "", 0, 87,
-                "","", 0.0, 0.0, 0, ""
+                "", "", 0.0, 0.0, 0, ""
             ),
             Sensor(
                 "76544898567", null, "", 0, 57,
-                "","", 0.0, 0.0, 0, ""
+                "", "", 0.0, 0.0, 0, ""
             ),
             Sensor(
                 "67898575446", null, "", 0, 77,
-                "","", 0.0, 0.0, 0, ""
+                "", "", 0.0, 0.0, 0, ""
             )
         )
 
         adapter.setItems(data)
     }
 
-    private fun setGraphData(chartLayout: Any) {
-        val entries = mutableListOf<BarEntry>()
-        val days = mutableListOf<String>()
-        val daysOfWeek = listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
-
-        val chart = when(chartLayout) {
-            is BarChart -> chartLayout
-            else -> chartLayout as LineChart
-        }
-
-        // create a dummy list of entries for each day of the week
-        for (i in 0..100) {
-            days.add(daysOfWeek[i % 7])
-            entries.add(BarEntry(i.toFloat(), (Math.random() * 100).toFloat()))
-        }
-
-        // a dataset for our bar chart
-        // TODO: using string resources for the label
-        val set1 = BarDataSet(entries, "Measured Precipitation")
-        // create a bar for each day of the week
-        set1.setDrawValues(false)  // remove the actual values being displayed on top of the bars
-
-        // TODO: change the color of the bars based on the value of the bar
-        set1.setColors(
-            resources.getColor(R.color.navy_blue, null),
-            resources.getColor(R.color.light_grey, null)
-        )
-
+    private fun styleBarGraph(chart: BarChart) {
         // add the name of the days on top of x-axis
-        chart.xAxis.valueFormatter = IndexAxisValueFormatter(days)
-
-        // create a data object with the data sets
-        val data = BarData(set1)
+        chart.xAxis.valueFormatter = IndexAxisValueFormatter(viewModel.days)
 
         chart.apply {
             xAxis.setDrawGridLines(false)
@@ -118,13 +118,37 @@ class GraphFragment : Fragment() {
             axisLeft.textColor = resources.getColor(android.R.color.darker_gray, null)
             description.isEnabled = false
             legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+            legend.form = Legend.LegendForm.CIRCLE
             xAxis.granularity = 1f  // set the minimum distance between labels
             legend.yOffset = 10f  // move the legend up a bit
-        }
+            setFitBars(true)
+            setVisibleXRangeMaximum(7f)
 
-        // set data
-        chart.data = data
-        chart.setVisibleXRangeMaximum(7f)
+        }
+    }
+
+    private fun styleLineGraph(chart: LineChart) {
+        // add the name of the days on top of x-axis
+        chart.xAxis.valueFormatter = IndexAxisValueFormatter(viewModel.days)
+
+        chart.apply {
+            xAxis.setDrawGridLines(false)
+            xAxis.textSize = 15f
+            extraTopOffset = 20f
+            extraBottomOffset = 20f
+            axisLeft.axisLineColor = resources.getColor(R.color.light_grey, null)
+            xAxis.setDrawAxisLine(false)
+            axisLeft.setDrawAxisLine(false)
+            axisRight.isEnabled = false
+            axisLeft.textColor = resources.getColor(android.R.color.darker_gray, null)
+            description.isEnabled = false
+            legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+            legend.form = Legend.LegendForm.CIRCLE
+            xAxis.granularity = 1f  // set the minimum distance between labels
+            legend.yOffset = 10f  // move the legend up a bit
+            setVisibleXRangeMaximum(7f)
+
+        }
     }
 
     private fun showDatePicker(editText: TextInputEditText) {
