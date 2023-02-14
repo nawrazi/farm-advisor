ca414cace1f6c1e3d93a9cbf01bddbb734170167
Bruk Tedla <biruk.tedla93@gmail.com>
2023-02-14 04:10:09 +0300

    revert ip address

--- app/src/main/java/com/enterprise/agino/common/Constants.kt
+++ app/src/main/java/com/enterprise/agino/common/Constants.kt
@@ -3,7 +3,7 @@ package com.enterprise.agino.common
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object Constants {
-    const val BASE_URL = "http://10.0.2.2:7071/"
+    const val BASE_URL = "http://192.167.4:7071/"
     const val MAP_KEY = "QKG5sZdSQCFSitlPKDsIHZ9FV7ZGZg4g"
 
     val FARM_PREF_KEY by lazy { stringPreferencesKey("farm") }
